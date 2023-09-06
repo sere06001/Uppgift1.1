406d4b1ec72bfae57bf18787be0390ad7a278683
sere06001 <sere06001@utb.vaxjo.se>
2023-09-06 10:05:50 +0200

    Add project files.

---
+++ Uppgift1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift1_1 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1."); // Denna kod skriver ut "Du är nästan klar med kapitel 1." i console
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\."); // Denna kod skriver ut "Du har lär dig skriva ut tecken som " och \" i console
+            Console.ReadKey();
+        }
+    }
+}
